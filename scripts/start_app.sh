a64397ac78c990132093fb46a5a44b43633542c8
onlineresultsite <footflux@gmail.com>
2024-07-13 18:02:47 +0530

    add

--- scripts/start_app.sh
+++ scripts/start_app.sh
@@ -2,11 +2,13 @@
 
 sed -i 's/\[]/\["13.53.132.185"]/' /home/ubuntu/ecom_main/ecom/settings.py
 
+cd /ecom_main
 python manage.py migrate 
 python manage.py makemigrations     
 python manage.py collectstatic
 sudo service gunicorn restart
 sudo service nginx restart
+cd /ecom_main
 #sudo tail -f /var/log/nginx/error.log
 #sudo systemctl reload nginx
 #sudo tail -f /var/log/nginx/error.log
