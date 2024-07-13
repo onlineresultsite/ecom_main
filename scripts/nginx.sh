8c1515e198e72c98bc4e67bba0ccf31b84e28252
onlineresultsite <footflux@gmail.com>
2024-07-13 16:52:04 +0530

    add

--- scripts/nginx.sh
+++ scripts/nginx.sh
@@ -1,25 +1,13 @@
+
 #!/usr/bin/bash
 
-# Reload systemd manager configuration
 sudo systemctl daemon-reload
-
-# Remove the default site configuration if it exists to prevent conflicts
 sudo rm -f /etc/nginx/sites-enabled/default
 
-# Copy the custom Nginx configuration to the sites-available directory
 sudo cp /home/ubuntu/ecom_main/nginx/nginx.conf /etc/nginx/sites-available/ecom
-
-# Remove any existing symbolic link to prevent duplicate entries
-sudo rm -f /etc/nginx/sites-enabled/ecom
-
-# Create a symbolic link from sites-available to sites-enabled
-sudo ln -s /etc/nginx/sites-available/ecom /etc/nginx/sites-enabled/
-
-# Add the www-data user to the ubuntu group (if needed)
+sudo ln -s /etc/nginx/sites-available/ecom/etc/nginx/sites-enabled/
+#sudo ln -s /etc/nginx/sites-available/blog /etc/nginx/sites-enabled
+#sudo nginx -t
 sudo gpasswd -a www-data ubuntu
-
-# Test Nginx configuration for syntax errors
-sudo nginx -t
-
-# Restart Nginx to apply changes
 sudo systemctl restart nginx
+
