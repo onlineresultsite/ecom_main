731a819914d4f282bfb50a062b8090a17573b900
onlineresultsite <footflux@gmail.com>
2024-07-14 23:10:15 +0530

    asdfds

--- scripts/start_app.sh
+++ scripts/start_app.sh
@@ -3,31 +3,16 @@
 # Update the settings.py file
 sed -i 's/\[]/\["13.53.132.185"]/' /home/ubuntu/ecom_main/ecom/settings.py
 
-# Navigate to the directory containing manage.py
-cd /home/ubuntu/ecom_main/
-
-# Check if manage.py exists
-if [ ! -f manage.py ]; then
-    echo "manage.py not found in /home/ubuntu/ecom_main/"
-    exit 1
-fi
-
-# Activate virtual environment
+cd /home/ubuntu/ecom_main
 source /home/ubuntu/env/bin/activate
 
-# Run Django management commands
-cd /home/ubuntu/ecom_main/
-python3 manage.py migrate
-cd /home/ubuntu/ecom_main/
-python3 manage.py makemigrations
-cd /home/ubuntu/ecom_main/
-python3 manage.py collectstatic --noinput
-cd /home/ubuntu/ecom_main/
-# Restart Gunicorn and Nginx services
+python manage.py migrate 
+python manage.py makemigrations     
+python manage.py collectstatic --noinput
+python manage.py runserver 0.0.0.0:8000
 sudo service gunicorn restart
-cd /home/ubuntu/ecom_main/
 sudo service nginx restart
-cd /home/ubuntu/ecom_main/
+python manage.py runserver 0.0.0.0:8000
 #sudo tail -f /var/log/nginx/error.log
 #sudo systemctl reload nginx
 #sudo tail -f /var/log/nginx/error.log
