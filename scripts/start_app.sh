650182bbeb9d97d3f080a3417cef6ee2ef9a3008
onlineresultsite <footflux@gmail.com>
2024-07-15 10:47:38 +0530

    dfsdf

--- scripts/start_app.sh
+++ scripts/start_app.sh
@@ -9,10 +9,19 @@ source /home/ubuntu/env/bin/activate
 python3 manage.py migrate 
 python3 manage.py makemigrations     
 python3 manage.py collectstatic --noinput
+
+sudo chown -R ubuntu:ubuntu /home/ubuntu/ecom_main/db.sqlite3
+sudo chown -R ubuntu:ubuntu /home/ubuntu/ecom_main
+sudo chmod 664 /home/ubuntu/ecom_main/db.sqlite3
+sudo chmod 775 /home/ubuntu/ecom_main
+sudo chmod 775 /home/ubuntu/ecom_main
+ls -l /home/ubuntu/ecom_main/db.sqlite3
+
 sudo service gunicorn restart
 sudo service nginx restart
+
 cd /home/ubuntu/ecom_main
-python3 manage.py runserver 0.0.0.0:8000
+# python3 manage.py runserver 0.0.0.0:8000
 #sudo tail -f /var/log/nginx/error.log
 #sudo systemctl reload nginx
 #sudo tail -f /var/log/nginx/error.log
